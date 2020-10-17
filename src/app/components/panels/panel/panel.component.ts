96c3374ee5a11f6d7f5e111c8f38d3ac8dc180fa
ttahir <ttahir@hala.com>
2020-10-17 20:03:50 +0300

    panel active class

--- src/app/components/panels/panel/panel.component.ts
+++ src/app/components/panels/panel/panel.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap/accordion/accordion';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
+  isPanelActive: boolean;
+  @ViewChild('myaccordion', {static : true}) accordion: NgbAccordion;
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  onPanelChange(evt) {
+   var selector = `#${evt.panelId}-header`;
+   this.accordion.panels.forEach(p => {
+     $(`#${p.id}-header`).removeClass('active');
+   });
+   if (evt.nextState)
+    $(selector).addClass('active');
+    if(!evt.nextState)
+    $(selector).removeClass('active');
+  }
 }
